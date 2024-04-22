659b93ee237e81896c76d8385acc420971f59f42
StonedOP <naman278sharma@gmail.com>
2024-04-22 21:31:46 +0530

    OOPs2 package a

---
+++ src/OOPs2/a/greeting.java
@@ -0,0 +1,13 @@
+package OOPs2.a;
+//here we can see after pressing (<alt + enter>)
+//java imported
+import static OOPs2.b.Message.message;
+
+//we have created 2 classes of name greeting in package a & b
+public class greeting {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        message();
+
+    }
+}
